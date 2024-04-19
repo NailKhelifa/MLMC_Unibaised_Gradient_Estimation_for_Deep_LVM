26673d6bfc977f675ec54c33cd98a14c9bedabab
NailKhelifa <nail.khelifa@ensae.fr>
2024-04-19 20:39:24 +0200

    minor change

--- Code.py
+++ Code.py
@@ -139,14 +139,12 @@ def single_sample(r, I_0, Delta, K):
 
     return I_0 + Delta(K)/(((1-r)**(K-1))*r)
     
-def l_hat(weights_array):
-
+'''def l_hat(weights_array):'''
 
 
 
 def log_likelihood_SUMO(r, theta, x, noised_A, noised_b, n_simulations):
-
-
+    
     SUMO_theta = []
 
     for _ in range(n_simulations):
@@ -167,3 +165,4 @@ def log_likelihood_SUMO(r, theta, x, noised_A, noised_b, n_simulations):
         ## On clacule l'estimateur de la roulette russe associé à ce delta, c'est celui qui correspond à l'estimateur SUMO 
         ## et on stocke le résultat dans la liste SUMO sur laquelle on moyennera en sortie 
         SUMO_theta.append(roulette_russe(I_0, Delta_theta, K))
+
