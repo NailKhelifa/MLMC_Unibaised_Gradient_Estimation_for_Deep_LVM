b43df91489927017fe6cae53d99c2cfb0627d3cb
NailKhelifa <nail.khelifa@ensae.fr>
2024-04-20 11:34:48 +0200

    final code log_likelihood_SUMO code.py

--- Code.py
+++ Code.py
@@ -168,7 +168,7 @@ def log_likelihood_SUMO(r, theta, x, noised_A, noised_b, n_simulations):
 
             pbar.update(1)  # Update the progress bar
 
-    return SUMO
+    return np.mean(SUMO)
 
 def log_likelihood_ML_SS(r, theta, x, noised_A, noised_b, n_simulations):
 
